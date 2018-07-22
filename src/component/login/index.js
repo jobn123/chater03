a122cf759fe2f81b5e8afd2bc1a19dacbea78e02
jobn123 <5910443987@qq.com>
2018-07-22 14:44:45 +0800

    add login

---
+++ src/component/login/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Login extends React.Component{
+
+  componentDidMount(){
+  }
+
+  render(){
+    return (
+      <div>xxxx</div>
+    )
+  }
+}
+
+export default Login
