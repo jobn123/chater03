e53a5e220905893b9fda489966154e8ec6ec24f5
ZhouBin <jobn_joy@163.com>
2018-08-01 17:23:46 +0800

    optimize group

--- src/pages/home/group.js
+++ src/pages/home/group.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { NavBar, Icon, SearchBar } from 'antd-mobile'
+import { SearchBar } from 'antd-mobile'
 import axios from "axios"
 import './group.css'
 
@@ -7,9 +7,11 @@ class Group extends React.Component{
   constructor() {
     super()
     this.state = {
-      value: "搜索影片名",
+      value: "",
       showSearchResults: false,
       searchResults: [],
+      groups: [],
+      selectMain: false
     }
   }
   onChange= (value) => {
@@ -49,12 +51,86 @@ class Group extends React.Component{
   goHomePage() {
     this.props.history.push('/')
   }
-  showSingle() {
+  addMainitem(item, index) {
+    let {selectMain, groups} = this.state
     
+    if (selectMain) return
+    let str = `group-heart_${index}`
+    let cname = this.refs[str].className
+    let arr = groups
+    
+    if (cname === "group-heart_gray") {
+      let obj = {
+        id: item.id,
+        title: item.title,
+        index: index
+      }
+      arr.unshift(obj)
+      this.setState({
+        selectMain: true
+      }, ()=>{
+        this.refs[str].className = "g-hearts"
+      })
+    } else {
+      this.refs[str].className = "group-heart_gray"
+      arr.shift()
+    }
+  }
+  delGroup(item, index) {
+    let arr = this.state.groups
+    arr.splice(index, 1)
+    if (index === 0 && this.state.selectMain) {
+      this.setState({
+        groups: arr,
+        selectMain: false
+      },()=>{
+        let str = `group-heart_${item.index}`
+        this.refs[str].className = "group-heart_gray"
+      })
+    }
+    this.setState({
+      groups: arr
+    }, ()=>{
+      let str = `group-add_${item.index}`
+      this.refs[str].className = "group-add_gray"
+    })
+  }
+  addGroups(item, index) {
+    let { groups } = this.state
+    //
+    let str = `group-add_${index}`
+    let cname = this.refs[str].className
+    let arr = groups
+
+    if (cname === "group-add_gray") {
+      if(arr.length > 7) return
+      let obj = {
+        id: item.id,
+        title: item.title,
+        index: index
+      }
+      arr.push(obj)
+
+      this.setState({
+        groups: arr
+      }, ()=>{
+        this.refs[str].className = "g-add"
+      })
+    } else {
+      this.refs[str].className = "group-add_gray"
+      arr.forEach((it, index) => {
+        if(it.id === item.id) {
+          arr.splice(index , 1)
+        }
+      });
+      this.setState({
+        groups: arr
+      })
+    }
   }
   renderBody() {
-    let { searchResults } = this.state
-  
+    let { searchResults, groups, selectMain } = this.state
+    
     if (searchResults.length === 0) {
       return (
         <div className="group-body_desc"> 
@@ -67,12 +143,28 @@ class Group extends React.Component{
       let arr = []
       for(let i = 0; i < searchResults.length; i++) {
         let cname = (i + 1) % 2 === 0 ? 's-item_g' : 's-item'
-        let item = (<div key={i} className={cname} onClick={()=>{this.showSingle(searchResults[i])}}>
-          {searchResults[i].title}
-          <i className="" />
-          <i className="" />
+        let item = (<div key={i} className={cname}>
+          <span style={{paddingRight: '16px'}}>{searchResults[i].title}</span>
+          <div className="groupicons">
+            <i className="group-heart_gray" ref={`group-heart_${i}`} onClick={()=>{this.addMainitem(searchResults[i], i)}} />
+            <i className="group-add_gray" ref={`group-add_${i}`} onClick={()=>{ this.addGroups(searchResults[i], i)}}/>
+          </div>
         </div>)
         arr.push(item)
+      }
+      //选择对比组
+      if (groups.length > 0) {
+        let garrs = []
+        for(let j = 0; j < groups.length; j++) {
+          let item = groups[j]
+          let cname = (j === 0 && selectMain) ? 'span-red' : 'span-blue'
+          let spanItem = (<span className={cname}>{item.title} <i onClick={()=>{this.delGroup(item, j)}} className="span-blue_i" /></span>)
+          garrs.push(spanItem)
+        }
+        return (<div>
+          <div>{garrs}</div>
+          { arr }
+        </div>)
       }
       return arr
     }
